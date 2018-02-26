c0c3094e1fa414381fc8689126882c036c6f154d
Kenneth Tilton <kentilton@gmail.com>
2018-02-26 18:16:55 -0500

    backup

--- js/matrix/js/examples/CPMatrixTodo.js
+++ js/matrix/js/examples/CPMatrixTodo.js
@@ -1,5 +1,40 @@
 var bits = new Array();
 
+var welcome = "Does the Web Framework Emperor have no clothes? Do we really \
+need tool chains as long as our arm? Must we explicitly publish and \
+subscribe to be reactive?\
+\n\
+Welcome to the development of an application within an application built \
+on nothing more than HTML, CSS, and a fine-grained, glitch-free, transparent, \
+dynamic data flow library.\
+\n\
+Impossible? Sixty-four submissions to TodoMVC cannot be in vain!\
+\n\
+Speaking of which, TodoMVC is the application we will be developing within \
+this Matrix/mxWeb&trade; application. See the end of this source for that code.\
+\n\
+Please hit 'Next' above to get started.";
+
+
+var bz = "\
+section({class: 'todoapp'},\n\
+   header({class: 'header'},\n\
+      h1('todos'))),\n\
+\n\
+   p('The working app will appear here.')";
+
+bits.push([
+    "Preface", welcome,
+    bz,
+    ["Pardon my CSS.",
+    "We have a ClojureScript version as well."],
+
+    [section({ class: "todoapp"},
+        header({class: "header"},
+            h1("todos"))),
+
+        p("The working app will appear here.")]]);
+
 var b0 = "\
 section({class: 'todoapp'},\n\
    header({class: 'header'},\n\
@@ -12,6 +47,7 @@ footer({class: 'info'},\n\
     .map( s => p({},s)))";
 
 bits.push([
+    "Title","blah",
     b0,
     ["mxWeb&trade; function signatures mirror HTML tag syntax.",
     "Meaning a graphic designer could learn mxWeb.",
@@ -51,6 +87,7 @@ function credits (attrs, ...content) {
 }
 
 bits.push([
+    "Title","blah",
     csCredits,
     ["This is just JS, so we can write our own DOM functions aping HTML syntax...",
         "...meaning the W3C can stop working on <a href='https://developer.mozilla.org/en-US/docs/Web/Web_Components'>Web Components</a>."],
@@ -106,6 +143,7 @@ section({ class: 'todoapp'},\n\
              c=> TodosLite.v.map( td => li( td.title))))";
 
 bits.push([
+    "Title","blah",
     enterTodos,
     ["New: changing data drives changing DOM population...",
         "...rather inefficiently for now, regenerating all LIs each time. We can fix that.",
@@ -145,6 +183,7 @@ function todoBetterAddNew (mx, e) {
 }
 
 bits.push([
+    "Title","blah",
     enterTodos,
     ["We drop the credits to save real estate."],
 
@@ -172,10 +211,26 @@ const bit = cFI( c=> {let r = window.localStorage.getObject("CPMatrixTodo.bit");
     // we use an observer to persist the current "bit" number so page reloads pick up where we left off
     { observer: (n, md, newv ) => window.localStorage.setObject("CPMatrixTodo.bit", newv)});
 
+function newsprint( text) {
+    let pgs = text.split("\n"),
+    brk = Math.ceil( pgs.length/2);
+    clg( "newsprint", pgs.length, brk);
+
+    return div( {style: {display: "flex",
+                        flex_direction: "row"},
+                class: "techwrite"},
+                div({style: {flex: "50%", padding: "10px"}},
+                    pgs.slice(0, brk).map( pgr => p(pgr))),
+                div({style: {flex: "50%", padding: "10px"}},
+                    pgs.slice(brk).map( pgr => p(pgr))));
+}
+
 function bitAssemble( bit) {
     var codeString, notes, code;
-    [codeString, notes, code] = bit;
+    [title, narrative, codeString, notes, code] = bit;
     return [
+        h2(title),
+        newsprint(narrative),
         div( {style: { margin_top: "36px",
                         display: "flex",
                         flex_direction: "row"}},
