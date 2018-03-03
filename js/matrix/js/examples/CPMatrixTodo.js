9bbd805b24c189c588be85853cf4e76ccb18b136
Kenneth Tilton <kentilton@gmail.com>
2018-03-03 11:20:05 -0500

    styling OK

--- js/matrix/js/examples/CPMatrixTodo.js
+++ js/matrix/js/examples/CPMatrixTodo.js
@@ -7,7 +7,8 @@ categories.map( c=> nubits[c] = {});
 
 var chats = {};
 var sources = {};
-var bitIds = ['preface', 'justhtml', 'webco', 'dynodom'];
+var bitIds = ['preface', 'justhtml', 'webco', 'dynodom', 'ktodo',
+                'xhr'];
 
 var bits = new Array;
 
@@ -43,6 +44,7 @@ function getcode( id) { return getit('code', id)}
 
 const bit = cFI( c=> {
         let r = window.localStorage.getObject("CPMatrixTodo.bit");
+        clg('local bit no ', r);
         return r === null ? 0 : (r < 0? 0: (r >= bitIds.length? bit.length -1: r));
     },
     // we use an observer to persist the current "bit" number so page reloads pick up where we left off
@@ -51,7 +53,7 @@ const bit = cFI( c=> {
 // --- main ------------------------------------------
 function CPMatrixTodo () {
     return [
-        h1("Introducing Matrix&trade; and mxWeb&trade;"),
+        p({class: 'techtitular techtitle'}, "Introducing Matrix and mxWeb"),
         toolbar(),
         div( c=> bitAssemble( bitIds[bit.v])),
         toolbar()
@@ -71,14 +73,14 @@ function bitAssemble( bid) {
 
     return [
         div( b.mxDom),
-        h2( b.title),
+        p( {class: 'techtitular techsubtitle'}, b.title),
         newsprint( chat),
-        b.notes? div( h3("Nota bene:"),
+        b.notes? div( p({class: 'techheader'}, "Nota bene"),
             ul( {class: "techwrite",
                     style: "list-style:square"},
                 b.notes.map( note=> li( {style: {margin_bottom: "6px"}},
                     note)))): null,
-        div( h3("Code Highlights"),
+        div( p({class: 'techheader'}, "Code Highlights"),
             pre({class: 'precode'}, codeString))
     ];
 }
@@ -194,6 +196,15 @@ defbit('justhtml',
             ].map(s => p({}, s)))]
     });
 
+/// --- webco - Web Components -------------------------------
+///
+
+function credits (attrs, ...content) {
+    return footer(Object.assign({}, {class: "info"}, attrs),
+        // Look, Ma! No JSX! No toolchain! Standard JS....
+        content.map( s => p({},s)));
+}
+
 defbit( "webco",
      {
         title: "Web Components? Done.",
@@ -249,6 +260,8 @@ function todoDashboardEZ ( ...plugins ) {
     );
 }
 
+/// dynodom - DOM driven by data flow ------------------------------------------------------
+
 defbit("dynodom",
     {
         title: "Enter Data Flow",
@@ -275,7 +288,7 @@ defbit("dynodom",
     });
 
 // ---------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------
+// --- ktodo - To-do gets its own data flow properties -----------------------------
 
 class Todo extends Model {
     constructor( title ) {
@@ -314,58 +327,7 @@ function todoLI( c, todo, extras) {
                 onclick: ()=> todo.deleted = true})));
 }
 
-var moFlowChat = "To-dos now have their own JS class along with individual Cell-powered properties, \
-and a fancier LI where those properties can be manipulated.\
-\n\
-Add a to-do now and examine the LI.\
-\n\
-The faint circle to the left is where you toggle whether a to-do has been completed. \
-If you toggle one, look for 'Clear completed' in the dashboard. That is a working button. \
-Give it a go, if you like.\
-\n\
-Remember, the spec says to hide the dashboard if there are no items, completed or not. \
-Watch for the dashboard to disappear when you delete the last remaining item.\n\
-\n\
-When you hover over a to-do, a red 'X' appears to the far right. Click that to \
-permanently delete a to-do item.\
-\n\
-As you play, keep an eye on 'items remaining'.";
 
-var moFlowCode = "\
-class Todo extends Model {\n\
-    constructor( title ) {\n\
-        super( null, null,\n\
-            {\n\
-                title: title,\n\
-                completed: cI( false),\n\
-                deleted: cI( false)\n\
-            });\n\
-    }\n\
-}\n\
-\n\
-function todoLI( c, todo) {\n\
-  return li({ <u>class: cF(c => (todo.completed ? 'completed' : null))}</u>,\n\
-    div({class: 'view'},\n\
-        input({\n\
-            class: 'toggle',\n\
-            type: 'checkbox',\n\
-            <u>checked: cF( c=> todo.completed),</u>\n\
-            <b><i>onclick: ()=> todo.completed = !todo.completed}</i></b>)\n\
-\n\
-        , label({ content: todo.title})\n\
-\n\
-        , button({ class: 'destroy',\n\
-                   <b><i>onclick: ()=> todo.deleted = true</i></b>})));\n\
-}\n\
-\n\
-function clearCompleted () {\n\
-    return button({ class: 'clear-completed',\n\
-                    <u>hidden: cF(c => !Todos.items.filter(td => td.completed).length)</u>,\n\
-            onclick: mx => Todos.items\n\
-                             .filter( td => td.completed )\n\
-                             .map( <b><i>td => td.deleted = true</i></b>)},\n\
-        'Clear completed');\n\
-}";
 
 function clearCompleted () {
     return button({ class: "clear-completed",
@@ -374,102 +336,56 @@ function clearCompleted () {
         "Clear completed");
 }
 
-bits.push(
-    function () {
-        return {
-            title: "To-Do properties join the data flow",
-            chat: moFlowChat,
-            code: moFlowCode,
-            notes: ["Reactive data flow now includes model as well as view.",
-                "Data flow connects the separate concerns of model and view.",
-                "A simple derived 'empty' property keeps us DRY."],
-            initFn: ()=> Todos = mkm( null, "Todos", {
-                items: cI( [new Todo( "Wash car")]),
-                empty: cF( c=> c.md.items.length===0)}),
-            mxDom: [
-                section({class: "todoapp"},
-                    todoAppHeader( todoAddNewBetter),
-                    section({
-                            class: "main",
-                            hidden: cF(c => Todos.empty)
-                        },
-                        ul({class: "todo-list"},
-                            c => Todos.items
-                                .filter(todo => !todo.deleted)
-                                .map(td => todoLI( c, td )))),
-                    todoDashboardEZ(clearCompleted))]
-        }
+defbit('ktodo',
+    {
+        title: "To-Do properties join the data flow",
+        notes: ["Reactive data flow now includes model as well as view.",
+            "Data flow connects the separate concerns of model and view.",
+            "A simple derived 'empty' property keeps us DRY."],
+        initFn: ()=> Todos = mkm( null, "Todos", {
+            items: cI( [new Todo( "Wash car")]),
+            empty: cF( c=> c.md.items.length===0)}),
+        mxDom: [
+            section({class: "todoapp"},
+                todoAppHeader( todoAddNewBetter),
+                section({
+                        class: "main",
+                        hidden: cF(c => Todos.empty)
+                    },
+                    ul({class: "todo-list"},
+                        c => Todos.items
+                            .filter(todo => !todo.deleted)
+                            .map(td => todoLI( c, td )))),
+                todoDashboardEZ(clearCompleted))]
     });
 
-var xhrCode = "\
-section({class: 'todoapp'},\n\
-   todoAppHeader( todoAddNewBetter),\n\
-   section({ class: 'main',\n\
-             hidden: cF(c => Todos.empty)},\n\
-      ul({ class: 'todo-list'},\n\
-         { kidValues: cF(c => Todos.items),\n\
-           kidKey: k => k.todo,\n\
-           kidFactory: (c,td) => todoLI(c, td, aeAlertGI)},\n\
-        c => c.kidValuesKids())),\n\
-      todoDashboardEZ(clearCompleted))\n\
-      \n\
-function aeAlertGI ( c, todo ) {\n\
-    return i( { class: 'aes material-icons md-36',\n\
-                style: 'font-size:36px;color:red;background:white',\n\
-                hidden: cF( c=> !c.md.aeInfo),\n\
-                onclick: mx => alert( mx.aeInfo)},\n\
-              { lookup: cF( c=> new mxXHR( aeBrandURI( todo.title),\n\
-                                           { send: true,\n\
-                                             delay: 500 + Math.random(5)*1000})),\n\
-    \n\
-                aeInfo: cF( function (c) {\n\
-                    let xhr = c.md.lookup.xhr;\n\
-                    if ( xhr) {\n\
-                        if ( xhr.isSuccess() ) {\n\
-                            let obj = xhr.getResponseJson();\n\
-                            return obj.meta.results.total + ' Adverse Events found on FDA.gov';\n\
-                        } else {\n\
-                            return null;\n\
-                        }\n\
-                    }\n\
-                })},\n\
-           'warning')\n\
-}\n\
-\n\
-function aeBrandURI (brand) {\n\
-    return `https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name:${ brand }&limit=3`\n\
-}\n\
-";
+// --- xhr ---------------------------------------------------------
 
-bits.push(
-    function () {
-        return {
-            title: "XHR joins the data flow",
-            chat: xhrChat,
-            code: xhrCode,
-            notes: [
-                "Reactive data flow now includes XHR as well as view.",
-                "The mxXHR 'lift' was hacked just enough to support this panel. More to come.",
-                "'kidValues' mechanism avoids even rebuilding existing proxies."],
-            initFn: ()=> Todos = mkm( null, "Todos", {
-                items: cI( ["adderall", "Yankees", "water", "aspirin"].map(td=> new Todo( td))),
-                empty: cF( c=> c.md.items.length===0)}),
-            mxDom: [
-                section({class: "todoapp"},
-                    todoAppHeader( todoAddNewBetter),
-                    section({
-                            class: "main",
-                            hidden: cF(c => Todos.empty)
+defbit('xhr',
+    {
+        title: "XHR joins the data flow",
+        notes: [
+            "Reactive data flow now includes XHR as well as view.",
+            "The mxXHR 'lift' was hacked just enough to support this panel. More to come.",
+            "'kidValues' mechanism avoids even rebuilding existing proxies."],
+        initFn: ()=> Todos = mkm( null, "Todos", {
+            items: cI( ["adderall", "Yankees", "water", "aspirin"].map(td=> new Todo( td))),
+            empty: cF( c=> c.md.items.length===0)}),
+        mxDom: [
+            section({class: "todoapp"},
+                todoAppHeader( todoAddNewBetter),
+                section({
+                        class: "main",
+                        hidden: cF(c => Todos.empty)
+                    },
+                    ul({class: "todo-list"},
+                        {
+                            kidValues: cF(c => Todos.items),
+                            kidKey: k => k.todo,
+                            kidFactory: (c,td) => todoLI(c, td, aeAlertGI)
                         },
-                        ul({class: "todo-list"},
-                            {
-                                kidValues: cF(c => Todos.items),
-                                kidKey: k => k.todo,
-                                kidFactory: (c,td) => todoLI(c, td, aeAlertGI)
-                            },
-                            c => c.kidValuesKids())),
-                    todoDashboardEZ(clearCompleted))]
-        }
+                        c => c.kidValuesKids())),
+                todoDashboardEZ(clearCompleted))]
     });
 
 function aeBrandURI (brand) {
@@ -512,25 +428,11 @@ function aeAlertSVG () {
             fill: "#000000",
             d: "M13,14H11V10H13M13,18H11V16H13M1,21H23L12,2L1,21Z"}))
 }
-// -------------------------------------------------------------
-var xhrChat = "Callback Hell? In the imperative paradigm, yes. \
-But the data flow paradigm is all about managing application state \
-gracefully after asynchronous inputs. So...XHR is right in the data flow wheelhouse:\
-\n\
-We drive this home by forcing a fake delay of two seconds. Whenever the \
-request returns, the response handler simply assigns the response to the \
-appropriate input Cell. Matrix internals then propagate the change as \
-with any other input.\
-\n\
-Here we pointlessly take the to-do item and look it up in the \
-FDA.gov adverse events database. If you see the warning icon, give \
-it a click.\
-\n\
-FDA.gov is aggressive about matching, so 'Wash car' will find results. \
-And all drugs have adverse events, so do not be concerned by <i>any</i> results.";
+
 
 // --------------------------------------------------------------------
 // ----- Text Content -------------------------------------------------
+
 defchat( 'preface',
     "Welcome to the development of an application within an application, each built \
 on just HTML, CSS, and a fine-grained data flow system we call Matrix.\
@@ -539,7 +441,7 @@ The application developed will cover half the classic \
 <a target='_blank' href='https://github.com/tastejs/todomvc/blob/master/app-spec.md'>TodoMVC spec</a> and \
 appear live above. Code highlights from the pen will appear below.\
 \n\
-The first hundred lines of the source comprise the host application.\
+The first hundred lines of the source comprise the host application, itself a nice demonstration of mxWeb.\
 \n\
 Please check the notes below, then hit 'Next' to get started.");
 
@@ -593,12 +495,6 @@ credits({style: 'font-size:18px'},\n\
    'Created by &lt;a href='http://tiltontec.com'&gt;Kenneth Tilton',\n\
    'Inspired by &lt;a href='http://todomvc.com'&gt;TodoMVC&lt;/a&gt;'])");
 
-function credits (attrs, ...content) {
-    return footer(Object.assign({}, {class: "info"}, attrs),
-        // Look, Ma! No JSX! No toolchain! Standard JS....
-        content.map( s => p({},s)));
-}
-
 defchat("webco",
     "Since we just code JS, developing a custom HTML element is as easy as writing a function. \
 That function can take whatever parameters we like to make it as reusable as we like. \
@@ -608,12 +504,18 @@ promising, but JS functions will be hard to top. \
 \n\
 The function 'credits' in the code below is a trivial example.");
 
+
+// -------------------------------------------------------------------------------------
+// -------------  dynodom --------------------------------------------------------------
+
 defcode('dynodom', "\
 Glossary:\n\
    - 'cI' creates an 'input' Cell.\n\
    - 'cF' creates a 'formula' Cell.\n\
    - 'mkm' makes a model (object with cells for properties)\n\
-\n\
+<i>where we compute from other data</i>\n\
+<b>where we compute or set data</b>\n\
+\n\n\
 Todos = mkm( null, 'Todos', {\n\
             items: cI( []),\n\
             <b>empty: cF( c=> <i>c.md.items</i>.filter( td=> !<i>td.deleted</i>).length ===0)</b>\n\
@@ -683,6 +585,126 @@ Try it, if you like; the app within the app is live.\n\
 \n\
 Note the transparency of the data flow in the code below. Well, you \
 cannot, it is transparent, so we highlighted the implicit pub/sub.");
+
+defchat('ktodo', "To-dos now have their own JS class along with individual Cell-powered properties, \
+and a fancier LI where those properties can be manipulated.\
+\n\
+Add a to-do now and examine the LI.\
+\n\
+The faint circle to the left is where you toggle whether a to-do has been completed. \
+If you toggle one, look for 'Clear completed' in the dashboard. That is a working button. \
+Give it a go, if you like.\
+\n\
+Remember, the spec says to hide the dashboard if there are no items, completed or not. \
+Watch for the dashboard to disappear when you delete the last remaining item.\n\
+\n\
+When you hover over a to-do, a red 'X' appears to the far right. Click that to \
+permanently delete a to-do item.\
+\n\
+As you play, keep an eye on 'items remaining'.");
+
+defcode('ktodo', "\
+<i>where we compute from other data</i>\n\
+<b>where we compute or set data</b>\n\
+\n\
+class Todo extends Model {\n\
+    constructor( title ) {\n\
+        super( null, null,\n\
+            {\n\
+                title: title,\n\
+                completed: cI( false),\n\
+                deleted: cI( false)\n\
+            });\n\
+    }\n\
+}\n\
+\n\
+function todoLI( c, todo) {\n\
+  return li({ <b>class: cF(c => (<i>todo.completed</i> ? 'completed' : null))}</b>,\n\
+    div({class: 'view'},\n\
+        input({\n\
+            class: 'toggle',\n\
+            type: 'checkbox',\n\
+            <b>checked: cF( c=> <i>todo.completed</i>),</b>\n\
+            <b>onclick: ()=> todo.completed = !todo.completed}</b>)\n\
+\n\
+        , label({ content: todo.title})\n\
+\n\
+        , button({ class: 'destroy',\n\
+                   <b>onclick: ()=> todo.deleted = true</b>})));\n\
+}\n\
+\n\
+function clearCompleted () {\n\
+    return button({ class: 'clear-completed',\n\
+                    <b>hidden: cF(c => !<i>Todos.items</i>.filter(td => <i>td.completed</i>).length)</b>,\n\
+            onclick: mx => Todos.items\n\
+                             .filter( td => td.completed )\n\
+                             .map( <b>td => td.deleted = true</b>)},\n\
+        'Clear completed');\n\
+}");
+
+defchat('xhr', "Callback Hell? In the imperative paradigm, yes. \
+But the data flow paradigm is all about managing application state \
+gracefully after asynchronous inputs. So...XHR is right in the data flow wheelhouse:\
+\n\
+We drive this home by forcing a fake delay of two seconds. Whenever the \
+request returns, the response handler simply assigns the response to the \
+appropriate input Cell. Matrix internals then propagate the change as \
+with any other input.\
+\n\
+Here we pointlessly take the to-do item and look it up in the \
+FDA.gov adverse events database. If you see the warning icon, give \
+it a click.\
+\n\
+FDA.gov is aggressive about matching, so 'Wash car' will find results. \
+And all drugs have adverse events, so do not be concerned by <i>any</i> results.\
+\n\
+This is a trivial callback scenario, but the data flow solution does scale \
+to complex scenarios. Here is my <a target=''_blank' href='https://github.com/kennytilton/xhr/blob/master/cljs/xhr/XHR.md'>\
+original investigation</a> into XHR via data flow.");
+
+
+// --- xhr --------------------------------------------------------
+defcode('xhr', "\
+<i>where we compute from other data</i>\n\
+<b>where we compute or set data</b>\n\
+\n\n\n\
+section({class: 'todoapp'},\n\
+   todoAppHeader( todoAddNewBetter),\n\
+   section({ class: 'main',\n\
+             <b>hidden: cF(c => <i>Todos.empty</i>)</b>},\n\
+      ul({ class: 'todo-list'},\n\
+         { <b>kidValues: cF(c => <i>Todos.items</i>)</b>,\n\
+           kidKey: k => k.todo,\n\
+           kidFactory: (c,td) => todoLI(c, td, aeAlertGI)},\n\
+        <b>c => <i>c.kidValuesKids()</i>)</b>),\n\
+      todoDashboardEZ(clearCompleted))\n\
+      \n\
+function aeAlertGI ( c, todo ) {\n\
+    return i( { class: 'aes material-icons md-36',\n\
+                style: 'font-size:36px;color:red;background:white',\n\
+                hidden: cF( c=> !c.md.aeInfo),\n\
+                onclick: mx => alert( mx.aeInfo)},\n\
+              { lookup: cF( c=> new mxXHR( aeBrandURI( todo.title),\n\
+                                           { send: true,\n\
+                                             delay: 500 + Math.random(5)*1000})),\n\
+    \n\
+                aeInfo: cF( function (c) {\n\
+                    let <i>xhr = c.md.lookup.xhr</i>;\n\
+                    if ( xhr) {\n\
+                        if ( xhr.isSuccess() ) {\n\
+                            let obj = xhr.getResponseJson();\n\
+                            return obj.meta.results.total + ' Adverse Events found on FDA.gov';\n\
+                        } else {\n\
+                            return null;\n\
+                        }\n\
+                    }\n\
+                })},\n\
+           'warning')\n\
+}\n\
+\n\
+function aeBrandURI (brand) {\n\
+    return `https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name:${ brand }&limit=3`\n\
+}");
 
 
 // document.body.innerHTML =  tag2html( page());
