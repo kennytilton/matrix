bfd6be0f38b187de1cb48a9039a180fc0e696143
Kenneth Tilton <kentilton@gmail.com>
2018-07-27 12:08:27 -0400

    rxtrak runs

--- cljs/rxtrak/src/rxtrak/core.cljs
+++ cljs/rxtrak/src/rxtrak/core.cljs
@@ -1,5 +1,12 @@
 (ns ^:figwheel-hooks rxtrak.core
-    (:require ))
+  (:require
+    [goog.dom :as dom]
+    [tiltontec.model.core :refer [<mget]]
+    [tiltontec.webmx.html :refer [tag-dom-create *webmx-trace*]]
+    ;;[todomx.build :as tmx]
+    [rxtrak.build :as rxtrak]
+    [taoensso.tufte :as tufte :refer (defnp p profiled profile)]
+    [cljs-time.coerce :refer [from-long to-string] :as tmc]))
 
 (enable-console-print!)
 
@@ -9,6 +16,17 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
+(tufte/add-basic-println-handler! {})
+
+(let [root (dom/getElement "tagroot")
+      app-matrix (rxtrak/matrix-build!)
+      app-dom (tag-dom-create
+                (<mget app-matrix :mx-dom))]
+
+  (set! (.-innerHTML root) nil)
+  (dom/appendChild root app-dom)
+  (when-let [route-starter (<mget app-matrix :router-starter)]
+    (route-starter)))
 
 ;; specify reload hook with ^;after-load metadata
 (defn ^:after-load on-reload []
