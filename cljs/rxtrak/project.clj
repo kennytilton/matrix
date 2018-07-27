13694b6e4d6287dfdc2d9c717b7f250ca50ec74c
Kenneth Tilton <kentilton@gmail.com>
2018-07-27 12:03:15 -0400

    project and index ok

--- cljs/rxtrak/project.clj
+++ cljs/rxtrak/project.clj
@@ -7,7 +7,18 @@
   :min-lein-version "2.7.1"
 
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.238"]]
+                 [org.clojure/clojurescript "1.10.238"]
+                 [com.cognitect/transit-cljs "0.8.243"]
+                 [com.andrewmcveigh/cljs-time "0.5.2"]
+                 [funcool/bide "1.5.0"]
+                 [clj-http "3.7.0"]
+                 [cljs-http "0.1.44"]
+                 [cheshire "5.8.0"]
+                 [com.taoensso/tufte "1.1.2"]
+                 ;; todo bring up to date
+                 [com.tiltontec/matrix "0.1.3-SNAPSHOT"]
+                 [com.tiltontec/xhr "0.1.1-SNAPSHOT"]
+                 [com.tiltontec/webmx "0.1.0-SNAPSHOT"]]
 
   :source-paths ["src"]
 
