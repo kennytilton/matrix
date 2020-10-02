a0b754694167d14304c9f639499106a5196435e1
Kenneth Tilton <kentilton@gmail.com>
2020-10-01 20:22:24 -0400

    better gitignore

---
+++ js/matrix/pen.js
@@ -0,0 +1,6 @@
+    div(
+      { style: { background: "ivory", padding: "24px" } },
+      h1("SimpleJX523"),
+      i("The simplicity of HTML..."),
+      p("...the <b>power</b> of an HLL.")
+    )
