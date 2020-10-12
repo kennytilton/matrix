4912ae6c89b216cc5475a85229d26d5767c4c539
Kenneth Tilton <kentilton@gmail.com>
2020-10-12 14:08:53 -0400

    obs prefs OK

--- cljs/whoshiring/src/whoshiring/preferences.cljs
+++ cljs/whoshiring/src/whoshiring/preferences.cljs
@@ -3,11 +3,15 @@
     [tiltontec.cell.core
      :refer-macros [cF cF+ c-reset-next! cFonce cFn]
      :refer [cI c-reset! make-cell]]
+    [tiltontec.cell.observer :as obs]
     [tiltontec.model.core
      :refer-macros [the-kids mdv!]
      :refer [fget mget fasc fm! make mset! backdoor-reset!]
      :as md]))
 
+(defmethod obs/observe-by-type [::preferences] [slot me newv oldv c]
+  (prn :OBS!!!! slot newv oldv ))
+
 (def prefs (md/make ::preferences
-             :app-help? (cI true)
+             :app-help? (cI false)
              :rgx-help? (cI false)))
