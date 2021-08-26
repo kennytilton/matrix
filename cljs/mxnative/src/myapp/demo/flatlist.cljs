239642c69695bdbc414a0af071ce907e7031af3a
Kenneth Tilton <kentilton@gmail.com>
2021-08-26 14:16:27 -0400

    all converted

--- cljs/mxnative/src/myapp/demo/flatlist.cljs
+++ cljs/mxnative/src/myapp/demo/flatlist.cljs
@@ -10,33 +10,7 @@
     ["react-native" :as rn]
     [helix.core :as hx :refer [defnc fnc $ <>]]
     [myapp.mxreact :as mxr :refer [mkrx mxu!]]
-    [myapp.mxrgen :as mxn :refer-macros [mkbox mkx mxfnc props]]))
-
-#_(defn input-new-todo []
-    (mkx rn/TextInput
-      :name :new-undo
-      :to-do (cI "")
-      :style (cF (clj->js {:height      40
-                           :margin      12
-                           :padding     10
-                           :backgroundColor
-                                        (if (even? (mget (mxr/mxu! me :my-counter) :counter))
-                                          "linen" "red")
-                           :borderWidth 1}))
-      :jsx {:&               (props [:value :to-do] :style)
-            :placeholder     "What needs doing?"
-            :autoFocus       true
-            :autoCapitalize  "sentences"
-            :multiline       false
-            :onEndEditing    #(prn :undo-on-end-editing
-                                (goog.object/getAllPropertyNames %))
-            :onSubmitEditing #(prn :undo-on-submit-editing
-                                (goog.object/getAllPropertyNames %))
-            :onChange        #(prn :undo-on-change
-                                (js->clj (goog.object/get % "nativeEvent")
-                                  :keywordize-keys true))
-            :onChangeText    #(do (prn :undo-bam-changetext %)
-                                  (mset! me :to-do %))}))
+    [myapp.mxrgen :as mxn :refer-macros [mkbox mxfnc props]]))
 
 ;;;
 ;;; Use this so SafeAreaView on Android stays away from status bar
@@ -70,7 +44,6 @@
                      :onValueChange       #(mswap! me :value not)
                      :ios_backgroundColor "#3e3e3e"
                      :trackColor          (js-obj "false" "#767577" "true" "#81b0ff")})
-
                   (mxn/TextInput
                     {:name     :new-undo
                      :to-do    (cI "")
@@ -81,18 +54,17 @@
                                     :borderWidth     1}
                      :disabled (cF (not (mget (mxu! me :counting?) :value)))}
                     {:&               (props [:value :to-do] :disabled :style)
-                     :placeholder     "What to doing?"
+                     :placeholder     "What to do?"
                      :autoFocus       true
                      :autoCapitalize  "sentences"
-                     :onChangeText    #(do (prn :undo-bam-changetext %)
-                                           (mset! me :to-do %))
+                     :onChangeText    #(mset! me :to-do %)
                      :onSubmitEditing #(let [n (js->clj (goog.object/get % "nativeEvent")
                                                  :keywordize-keys true)]
-                                         (prn :undo-on-submit-editing n)
                                          (when-not (str/blank? (:text n))
                                            (mswap! (mxu! me :todos-container) :todo-items
                                              conj {:key   (str (.now js/Date))
                                                    :title (:text n)})
+                                           ;; now clear input...
                                            (mset! me :to-do "")))})
 
                   (mxn/SafeAreaView
@@ -113,31 +85,4 @@
                                                       :marginVertical   8
                                                       :marginHorizontal 16}}
                                          ($ rn/Text {:style #js {:fontSize 32}}
-                                           (.-title (.-item i)))))}))
-                  )))))
-
-#_(mkrx
-    {:name       :todos-container
-     :todo-items (cI nil)
-     :rendering  (cF (let [items (mget me :todo-items)]
-                       ;; need to ^^ establish dependency
-                       ;; todo can we just do (fnc [(mget me :todo-items)]??
-                       (prn :items!!!!!! items)
-                       ($ (mxfnc
-                            ($ rn/SafeAreaView
-                              {:style (clj->js {:flex      1
-                                                :marginTop 0})}
-                              ($ rn/FlatList
-                                {:data         (clj->js (mget me :todo-items))
-                                 :keyExtractor (fn [i]
-                                                 (prn :keyex-sees i)
-                                                 (.-key i))
-                                 :renderItem   (fn [i]
-                                                 (prn :rendering-i i)
-                                                 ($ rn/View
-                                                   {:style #js {:backgroundColor  "#f9c2ff"
-                                                                :padding          9
-                                                                :marginVertical   8
-                                                                :marginHorizontal 16}}
-                                                   ($ rn/Text {:style #js {:fontSize 32}}
-                                                     (.-title (.-item i)))))}))))))})
+                                           (.-title (.-item i)))))})))))))
