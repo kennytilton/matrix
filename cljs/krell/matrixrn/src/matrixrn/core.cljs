6f70567e96a7214383a5a5d0e43e6c1c8abf3970
Kenneth Tilton <kentilton@gmail.com>
2021-09-02 19:46:52 -0400

    flatlist OK

--- cljs/krell/matrixrn/src/matrixrn/core.cljs
+++ cljs/krell/matrixrn/src/matrixrn/core.cljs
@@ -7,7 +7,8 @@
                      fget mxi-find mxu-find-type
                      kid-values-kids] :as md]
             [matrixrn.mxreact :as mxr :refer [mxu!]]
-            [matrixrn.demo.simple :as simple]))
+            [matrixrn.demo.simple :as simple]
+            [matrixrn.demo.flatlist :as flat]))
 
 (def <> react/createElement)
 
@@ -15,27 +16,7 @@
   (reset! mxr/ssdict {})
   (reset! mxr/refdict {})
   (reset! matrix
-    (simple/demo)))
-
-;(defn hello []
-;  (<> rn/View
-;      #js {:style #js {:backgroundColor "#FFFFFF"
-;                       :flex 1
-;                       :justifyContent "center"}}
-;      (<> rn/Text
-;          #js {:style #js {:color "black"
-;                           :textAlign "center"}}
-;          (str "Hello, world."))))
-
-;(defn figwheel-rn-root []
-;  (prn :building-new-matrix!!!!)
-;  (let [app-matrix (matrix-build!)                          ;; memoize?
-;        root (mget app-matrix :rx-dom)
-;        _ (prn :root!!!! root)
-;        root-element #_(renderfn {}) (mget root :rendering)
-;        ]
-;    (prn :root-elt root-element)
-;    root-element))
+    (flat/demo)))
 
 (defn ^:export -main [& args]
   ;; (hello)
