6cd9d07c24547444b1655fdb049b42acfc945d56
Kenneth Tilton <kentilton@gmail.com>
2021-09-05 09:21:10 -0400

    shadow matrixrn OK

--- cljs/shadow/matrixrn/src/main/mxrn/demo/simple.cljs
+++ cljs/shadow/matrixrn/src/main/mxrn/demo/simple.cljs
@@ -12,7 +12,7 @@
     [react-native-elements :as rne]
     ;["@react-navigation/native" :refer [NavigationContainer] :as rnav]
 
-    [mxrn.mxreact :as mxr :refer [mxu!]]
+    [mxrn.mxreact :as mxr :refer [mxu! mx*]]
     [mxrn.mxrgen :as mxn :refer-macros [mxfnc props]]))
 
 (def <> react/createElement)
@@ -26,11 +26,11 @@
                   #js {:style #js {:backgroundColor "#444"
                                    :flex            1
                                    :justifyContent  "center"}}
-                  #_ (mxn/Icon {}
-                    #js {:name "heartbeat"
-                         :type "font-awesome"
-                         :color "#f50"
-                         :onPress #(prn :thump)})
+                  #_(mxn/Icon {}
+                      #js {:name    "heartbeat"
+                           :type    "font-awesome"
+                           :color   "#f50"
+                           :onPress #(prn :thump)})
                   (mxn/Switch
                     {:name       :counting?
                      :value      (cI true)
@@ -47,7 +47,7 @@
                   (mxn/Button
                     {:name     :bam-button
                      :counter  (cI (rand-int 9999))
-                     :title    (cF (str "KaBoom is = " (mget me :counter)))
+                     :title    (cF (str "Kaboom is = " (mget me :counter)))
                      :disabled (cF (not (mget (mxu! me :counting?) :value)))}
                     (clj->js {:title    (mget me :title)
                               :disabled (mget me :disabled)
@@ -58,20 +58,23 @@
                   (mxn/Button
                     {:name  :dumper
                      :title (cF (str "Downer" (mget (mxu! me :bam-button) :counter)))}
-                    #js {:title   "hunh"                    ;; (mget me :title)
+                    #js {:title   "Booya"                   ;; (mget me :title)
                          :color   "red"
                          :onPress #(mswap! (mxu! me :bam-button) :counter dec)})
 
-                  (mxn/ActivityIndicator {}
-                    #js {:size  "large"
+
+                  (mxn/ActivityIndicator
+                    {:animating (cF (mget (mx* me :counting?) :value))}
+                    #js {:animating (mget me :animating)
+                         :size  "large"
                          :color "#00ff00"})
 
                   (mxn/Pressable
                     {:name   :presser
                      :clicks (cI 3)}
-                    (clj->js {:onPressIn #(prn :pressed-In!!  )
-                              :onPress #(do (prn :pressed!!  )
-                                            (mswap! me :clicks inc))})
+                    (clj->js {:onPressIn #(prn :pressed-In!!)
+                              :onPress   #(do (prn :pressed!!)
+                                              (mswap! me :clicks inc))})
                     (mxn/Text
                       {:name :an-item}
                       #js {:style #js {:width 96 :padding 12 :margin 6 :backgroundColor "yellow"}}
@@ -79,40 +82,40 @@
 
                   (mxn/View {}
                     #js {:style #js {;; :flex 1
-                                 :flexDirection   "row"
-                                 :backgroundColor "green"
-                                 :width           300
-                                 :height          48
-                                 :alignItems      "center"}}
-                   #_ (mxn/Text
-                      {:name :an-item}
-                      #js {:style #js {:width 96 :padding 12 :margin 6 :backgroundColor "yellow"}}
-                      (mxn/strng (str "hit me? " (mget (mxu! me :presser) :clicks))))
+                                     :flexDirection   "row"
+                                     :backgroundColor "green"
+                                     :width           300
+                                     :height          48
+                                     :alignItems      "center"}}
+                    #_(mxn/Text
+                        {:name :an-item}
+                        #js {:style #js {:width 96 :padding 12 :margin 6 :backgroundColor "yellow"}}
+                        (mxn/strng (str "hit me? " (mget (mxu! me :presser) :clicks))))
 
                     (mxn/SliderRNE
                       {:name        :slido
                        :slide-value (cI 0.3 :obs (fn [slot me newv oldv c]
-                                                   #_ (prn :slider-now slot newv oldv c)))}
+                                                   #_(prn :slider-now slot newv oldv c)))}
                       (clj->js {:value         (mget me :slide-value)
-                                :name         (mget me :name)
+                                :name          (mget me :name)
                                 :style         (clj->js {:flex 2})
                                 :maximumValue  50
                                 :minimumValue  10
                                 :step          1
                                 :trackStyle    (clj->js {:height 10 :color "green"})
-                                :thumbStyle    (clj->js {:height 30 :width 20 #_#_ :color "blue" :backgroundColor "cyan"})
-                                :onValueChange #(do ;;(prn :Sliding! %)
-                                                    (mset! me :slide-value %))}))))))))
+                                :thumbStyle    (clj->js {:height 30 :width 20 #_#_:color "blue" :backgroundColor "cyan"})
+                                :onValueChange #(do         ;;(prn :Sliding! %)
+                                                  (mset! me :slide-value %))}))))))))
 
-#_ (mxn/SafeAreaView
-     {:name :item-list}
-     {:style #js {:backgroundColor "yellow"}}
-     (for [n (range (mget (mxr/mxu! me :my-counter) :counter))]
-       (mxn/Text
-         {:name :an-item}
-         {:key   n
-          :style #js {:padding 4 :margin 5}}
-         (mxn/strng (str "Text " n)))))
+#_(mxn/SafeAreaView
+    {:name :item-list}
+    {:style #js {:backgroundColor "yellow"}}
+    (for [n (range (mget (mxr/mxu! me :my-counter) :counter))]
+      (mxn/Text
+        {:name :an-item}
+        {:key   n
+         :style #js {:padding 4 :margin 5}}
+        (mxn/strng (str "Text " n)))))
 
 #_(mxn/View {}
     {:style #js {:flex            1
