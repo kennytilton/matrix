cdba7d5b0710f8368fa8b0cf5c2ccb547c6e015f
Kenneth Tilton <kentilton@gmail.com>
2021-09-07 14:17:27 -0400

    take6 ns cleanup

--- cljs/shadow/matrixrn/src/main/matrixrn/app.cljs
+++ cljs/shadow/matrixrn/src/main/matrixrn/app.cljs
@@ -1,13 +1,9 @@
 (ns matrixrn.app
-  (:require [tiltontec.model.core
-             :refer-macros [cFkids with-par]
-             :refer [matrix mx-par mget mset! mswap!
-                     fget mxi-find mxu-find-type
-                     kid-values-kids] :as md]
+  (:require [tiltontec.model.core :as md]
             [expo.root :as expo-root]
             ["react" :as r]
             ["react-native" :as rn]
-            [matrixrn.matrixrn :as mxr :refer [mxu!]]
+            [matrixrn.matrixrn :as mxn :refer [mxu!]]
             ;[matrixrn.demo.simple :as demo]
             ;[matrixrn.demo.navi :as demo]
             ;[matrixrn.demo.flatlist :as demo]
@@ -21,18 +17,18 @@
 (def <> r/createElement)
 
 (defn matrix-build! []
-  (reset! mxr/ssdict {})
-  (reset! matrix (demo/demo)))
+  (reset! mxn/ssdict {})
+  (reset! md/matrix (demo/demo)))
 
 (defn ;; ^:dev/after-load
   start []
   (let [app-matrix (matrix-build!)
-        root-mx (mget app-matrix :rx-dom)
+        root-mx (md/mget app-matrix :rx-dom)
         _ (prn :root-mmmmmmx!!!! root-mx)
-        root-element (mget root-mx :react-element)]
+        root-element (md/mget root-mx :react-element)]
     (prn :root-elt root-element)
     (expo-root/render-root
-      (fn [] (mget root-mx :react-element)))))
+      (fn [] (md/mget root-mx :react-element)))))
 
 (defn init []
   (start))
