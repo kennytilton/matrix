c80a3ae3fddecc7be22ef35a438d29e9c0c98a48
Kenneth Tilton <kentilton@gmail.com>
2021-09-28 18:38:33 -0400

    ok after ditching todomvc

--- cljs/mxreact/src/todo_mvc/core.cljs
+++ cljs/mxreact/src/todo_mvc/core.cljs
@@ -1,17 +1,8 @@
 (ns todo-mvc.core
   (:require
-   [clojure.string :as string]
-   ;[helix.core :as hx :refer [$ <>]]
-   ;[helix.dom :as d]
-   ;[helix.impl.props :as impl.props]
-   ;[helix.hooks :as hooks]
-   ;[todo-mvc.components :as c]
-   ;;[todo-mvc.lib :refer [defnc]]
-   ;[todo-mvc.storage :as storage]
    [tiltontec.model.core :as md]
    [mxreact.mxreact :as mxr :refer [$]]
-   ["react-dom" :as rdom]
-   ["react-router-dom" :as rr]))
+   ["react-dom" :as rdom]))
 
 ; (.createElement
 ;                        (hx/get-react)
@@ -19,16 +10,9 @@
 ;                        (impl.props/dom-props ~(first args))
 ;                        ~@(rest args))
 
-(defn App
-  []
-  ;; (d/p "Hi fn mom 10")
-  ;; (d/$d "p" (str "Hi mom " (rand-int 99999)))
-  #_ (.createElement (mxr/get-react)
-    "p"
-    (clj->js {})
-    (str "Hi mom " (rand-int 99999)))
+(defn App []
   ($ :div {} (str "Bingo " (rand-int 99999))))
 
-(defn ^:dev/after-load start
-  []
+(defn ^:dev/after-load ;; todo try losing this once in react land
+  start []
   (rdom/render ($ App) (js/document.getElementById "app")))
