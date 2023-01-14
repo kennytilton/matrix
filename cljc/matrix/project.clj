6079f247998c24d180745e79721fa220e7d6485b
Kenneth Tilton <kentilton@gmail.com>
2023-01-14 05:57:55 -0500

    switch to snapshot semver for clojars

--- cljc/matrix/project.clj
+++ cljc/matrix/project.clj
@@ -1,4 +1,4 @@
-(defproject tiltontec/matrix "4.2.0"
+(defproject tiltontec/matrix "4.2.0-SNAPSHOT"
   :description "Matrix dataflow library for CLJ/CLJS"
   :scm {:name "git"
         :url "https://github.com/kennytilton/matrix"}
