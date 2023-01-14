6d5c9cac1c852b3dba600e53e7407ef1bc5172dd
Kenneth Tilton <kentilton@gmail.com>
2023-01-14 05:55:35 -0500

    version 4.2.0 adds with-mx

--- cljc/matrix/project.clj
+++ cljc/matrix/project.clj
@@ -1,4 +1,4 @@
-(defproject tiltontec/matrix "4.1.8-SNAPSHOT"
+(defproject tiltontec/matrix "4.2.0"
   :description "Matrix dataflow library for CLJ/CLJS"
   :scm {:name "git"
         :url "https://github.com/kennytilton/matrix"}
