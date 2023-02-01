3c34a44308115a1cae2bef45ac94e6586e0e5a76
Kenneth Tilton <kentilton@gmail.com>
2023-02-01 13:23:44 -0500

    dag-dump 1.0

--- cljc/matrix/project.clj
+++ cljc/matrix/project.clj
@@ -1,17 +1,17 @@
 (defproject com.tiltontec/matrix "4.2.1-SNAPSHOT"
   :description "Matrix dataflow library for CLJ/CLJS"
   :scm {:name "git"
-        :url "https://github.com/kennytilton/matrix"}
+        :url  "https://github.com/kennytilton/matrix"}
   :url "http://tiltontec.com"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/clojurescript "1.10.773"]
                  [org.clojure/core.async "1.6.673"]
                  [org.clojure/data.int-map "1.0.0"]
                  [com.cognitect/transit-cljs "0.8.264"]
-
+                 [com.taoensso/nippy "3.2.0"]
                  [clj-http "3.10.3"]
                  [cheshire "5.10.0"]
                  [org.clojure/java.data "1.0.95"]
@@ -20,28 +20,28 @@
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :plugins [[lein-cljsbuild "1.1.8"]
             [lein-npm "0.6.2"]]
-  
+
   :profiles {:dev {:dependencies [[cider/piggieback "0.5.1"]]
                    :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}}
 
   :test-paths ["test"]
   :npm {:dependencies [[source-map-support "0.4.0"]]}
-  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org/repo"}]
+  :deploy-repositories [["releases" {:sign-releases false :url "https://clojars.org/repo"}]
                         ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]]
   :source-paths ["src" "target/classes"]
   :clean-targets ["out" "release"]
   :target-path "target"
-  :cljsbuild {:builds {;; [note to self: go back to 5/26 on rube repo for minify]
-                       ;;
-                       :mxdev {:source-paths ["src"]
-                                  :compiler {:output-to "resources/public/js/main.js"
-                                             :output-dir "resources/public/js/out"
-                                             :optimizations :whitespace}}
-                       :mxtest {:source-paths ["src" "test"]
-                                   :compiler {:output-to "resources/public/js/main-test.js"
-                                              :optimizations :whitespace
-                                              :pretty-print true}}
-                       }
+  :cljsbuild {:builds        {;; [note to self: go back to 5/26 on rube repo for minify]
+                              ;;
+                              :mxdev  {:source-paths ["src"]
+                                       :compiler     {:output-to     "resources/public/js/main.js"
+                                                      :output-dir    "resources/public/js/out"
+                                                      :optimizations :whitespace}}
+                              :mxtest {:source-paths ["src" "test"]
+                                       :compiler     {:output-to     "resources/public/js/main-test.js"
+                                                      :optimizations :whitespace
+                                                      :pretty-print  true}}
+                              }
               :test-commands {"unit" ["phantomjs"
                                       "resources/test/phantom/runner.js"
                                       "resources/test/test.html"]}})
--- cljc/matrix/src/tiltontec/cell/core.cljc
+++ cljc/matrix/src/tiltontec/cell/core.cljc
@@ -1,6 +1,7 @@
 (ns tiltontec.cell.core
   (:require
 
+    [clojure.string :as str]
     [tiltontec.util.core
      :refer [any-ref? type-of err rmap-setf rmap-meta-setf pln]]
 
@@ -369,3 +370,67 @@ execution as soon as the current change is manifested."
 (defmacro with-mx [&  body]
   `(call-with-mx
      (fn [] ~@body)))
+
+;;; --- dag dump utility ----------------------------------------
+
+
+(def ^:dynamic *dag-depth*
+  "How far we are in edges from the starting node."
+  0)
+
+(def ^:dynamic *dag-prn-len*
+  "How many edges to follow from users/callers."
+  5)
+
+
+
+(def +dag-visited+
+  "Which DAG nodes have been dumped so far."
+  (atom #{}))
+
+(defn dag-prn [& os]
+  (let [path (apply str (repeat *dag-depth* ".|"))]
+    (apply println path os)))
+
+(def ^:dynamic *dag-node-only-printer*
+  (fn [tag c] (dag-prn tag :PM! (c-slot-name c) :of (c-md-name c))))
+
+(declare dag-dump-1)
+
+(defn dag-dump-callers [c]
+  (let [ccs (take (or *dag-prn-len* 999)
+              (c-callers c))]
+    (when (seq ccs)
+      (binding [*dag-depth* (inc *dag-depth*)]
+        (doseq [cc ccs]
+          ;; (dag-prn :used-by (c-slot-name cc) :of (c-md-name cc))
+          (dag-dump-1 :used-by cc))))))
+(def ^:dynamic *dag-callers-printer* dag-dump-callers)
+
+(defn dag-dump-useds [c]
+  (let [ccs (take (or *dag-prn-len* 999)
+              (c-useds c))]
+    (when (seq ccs)
+      (binding [*dag-depth* (inc *dag-depth*)]
+        (doseq [cc ccs]
+          ;; (dag-prn :using (c-slot-name cc) :of (c-md-name cc))
+          (dag-dump-1 :using cc))))))
+(def ^:dynamic *dag-useds-printer* dag-dump-useds)
+
+(defn dag-dump-1 [tag c]
+  (cond
+    (contains? @+dag-visited+ c)
+    (dag-prn (str/upper-case (str tag ": "(c-slot-name c) "/" (c-md-name c))))
+    :else (do
+            (swap! +dag-visited+ conj c)
+            (when-let [p *dag-node-only-printer*]
+              (p tag c))
+            (when-let [p *dag-callers-printer*]
+              (p c))
+            (when-let [p *dag-useds-printer*]
+              (p c)))))
+
+(defn dag-dump [tag c]
+  (reset! +dag-visited+ #{})
+  (binding [*dag-depth* 0]
+    (dag-dump-1 tag c)))
